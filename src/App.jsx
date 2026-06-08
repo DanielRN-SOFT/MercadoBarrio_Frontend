75caa217874d2912b715d02becd9a24e15d4ab94
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-06-07 22:15:31 -0500

    Inicializacion base del proyecto: MercadoBarrio

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+  <>
+  <div className='badge badge-primary'>
+    <p className='text-white'>Hello world</p>
+  </div>
+  </>
+  )
+}
+
+export default App
