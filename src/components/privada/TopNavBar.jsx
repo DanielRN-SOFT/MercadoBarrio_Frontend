1bd34cc51e6d713c2023bb3aa12d71d382200469
DanielRN-SOFT <daniel20ramirez06@gmail.com>
2026-06-27 07:53:54 -0500

    Dropdown en el perfil

--- src/components/privada/TopNavBar.jsx
+++ src/components/privada/TopNavBar.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import useAuth from "../../hooks/useAuth";
+import { MdOutlinePersonOutline, MdStore } from "react-icons/md";
+import { PiUser } from "react-icons/pi";
 
 const TopNavBar = ({ title = "Dashboard" }) => {
   return (
@@ -14,10 +16,6 @@ const TopNavActions = () => (
   <div className="flex items-center gap-6">
     <button className="text-on-surface-variant hover:text-primary transition-colors">
       <span className="material-symbols-outlined">notifications</span>
-    </button>
-    <button className="flex items-center gap-2 text-on-surface-variant hover:text-primary transition-colors text-label-md font-semibold">
-      <span className="material-symbols-outlined">help</span>
-      Soporte
     </button>
     <TopNavUser />
   </div>
@@ -37,18 +35,40 @@ const TopNavUser = () => {
   };
 
   return (
-    <div className="flex items-center gap-3 border-l pl-6 border-outline-variant">
-      <div className="text-right hidden md:block">
-        <p className="text-label-md font-semibold text-on-surface">
-          {auth.name}
-        </p>
-        <p className="text-label-sm text-secondary">Propietario</p>
+    <div className="dropdown cursor-pointer mx-2">
+      <div
+        tabIndex={0}
+        role="button"
+        className="flex items-center gap-3 border-l pl-6 border-outline-variant"
+      >
+        <div className="text-right hidden md:block">
+          <p className="text-label-md font-semibold text-on-surface">
+            {auth.name}
+          </p>
+          <p className="text-label-sm text-secondary">Propietario</p>
+        </div>
+        <div className="w-10 h-10 rounded-full ring-2 ring-primary-fixed bg-primary flex items-center justify-center">
+          <span className="text-sm font-semibold text-on-primary">
+            {getInitials(auth.name)}
+          </span>
+        </div>
       </div>
-      <div className="w-10 h-10 rounded-full ring-2 ring-primary-fixed bg-primary flex items-center justify-center">
-        <span className="text-sm font-semibold text-on-primary">
-          {getInitials(auth.name)}
-        </span>
-      </div>
+      <ul
+        tabIndex="-1"
+        className="dropdown-content menu bg-on-primary rounded-box z-1 w-40 p-2 shadow-sm"
+      >
+        <li className="py-0.5">
+          <Link to={"/perfil"} className="text-primary">
+            <MdOutlinePersonOutline className="text-2xl" /> Perfil
+          </Link>
+        </li>
+        <li className="py-0.5">
+          <Link to={"/inicio"} className="text-primary">
+            <MdStore className="text-2xl" />
+            Directorio
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 };
