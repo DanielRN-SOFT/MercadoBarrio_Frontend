827ff62770e0b9b16ee6f12bb79f0191470bd452
Mateo Hoyos <mateohoyosgamer@gmail.com>
2026-06-29 13:56:42 -0500

    Agregar prop name al componente Input

--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,7 +1,8 @@
-const Input = ({ placeholder, type, id, value, onChange }) => {
+const Input = ({ placeholder, type, id, name, value, onChange }) => {
   return (
     <input
       id={id}
+      name={name}
       value={value}
       className="input input-bordered border-outline-variant bg-surface focus:border-primary focus:ring-1 focus:ring-primary w-full font-body-md text-body-md rounded-full"
       placeholder={placeholder}
